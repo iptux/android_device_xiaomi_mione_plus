2b7f9722ba457c60ee6975f89797f60b0dc208ea
iptux <iptux7@gmail.com>
2012-12-28 06:45:42 +0800

    mione_plus: optimize dalvik vm

--- mione_plus.mk
+++ mione_plus.mk
@@ -84,3 +84,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapsize=96m \
+    dalvik.vm.dexopt-flags=m=y
+
