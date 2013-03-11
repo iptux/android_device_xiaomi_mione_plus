4462b3feb3c838e2877edeb29a377d381c9979c2
oiuyz2008 <oiuyz2008@gmail.com>
2013-03-11 17:52:32 +0800

    change name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 # Recovery
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_CUSTOM_GRAPHICS := ../../../device/xiaomi/mione_plus/recovery/graphics.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/xiaomi/mione_plus/recovery/graphics_cn.c
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_LUNFILE :="/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_HAS_DUAL_SYSTEM := true
