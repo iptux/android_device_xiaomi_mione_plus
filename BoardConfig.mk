2dfa902e99362577d79511858d30f95838b3d071
Tommy Alex <iptux7@gmail.com>
2013-04-06 16:46:58 +0800

    mione_plus: set LUN file path
    Change-Id: I24af8dc86a9fd228137444ae8d2900c88a827576

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,4 +80,6 @@ BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_CUSTOM_GRAPHICS := ../../../device/xiaomi/mione_plus/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 
